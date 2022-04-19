41ea457b4a90bb5ba9d15e4d7f9ac985e7f33e8a
macbeth <layne.wegenast@gmail.com>
2022-04-18 21:54:51 -0500

    post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,27 @@
 // CRUD (operation - interacting with the database - Proxy db.json)
+const app = require('express').Router(); 
+const fs = require('fs');
+
+const  notes  = require('../db/db.json');
+
+app.get ('/api/notes', (req, res) => {
+    notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    console.log('GEET Route',notes)
+    res.json(notes)
+});
+
+
+//New Data - C Create
+app.post ('/api/notes', (req, res) => {
+     notes.push({
+         id: Math.floor(Math.random() * 123),
+         title: req.body.title,
+         text: req.body.text
+     })
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes),function(error){
+        if (error) throw error;
+    })
+    console.log('POST Route',notes)
+    res.json(notes)
+});
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const fs = require('fs');
 
 const port = process.env.PORT || 3001;
 const app = express();
-
-const { notes } = require('./db/db.json');
-
 
 app.listen (PORT, () => {
     console.log(`API server is now on ${PORT}`);
