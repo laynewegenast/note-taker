453ec11e00285a959843f2fda638998328e97e03
macbeth <layne.wegenast@gmail.com>
2022-04-18 22:07:13 -0500

    add routes to server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({extended:true})),
+app.use(express.json())
+app.use(express.static("public"))
+
+var apiRoutes = require("./routes/apiRoutes")
+app.use(apiRoutes);
+
+var htmlRoutes = require("./routes/htmlRoutes")
+app.use(htmlRoutes);
 
 app.listen (PORT, () => {
     console.log(`API server is now on ${PORT}`);
